1d3b70e1706d569ae76eaa22448a289fd4f8d865
leydayet <leydy@ley.com>
2020-10-16 01:31:46 -0500

    version1

---
+++ funciones.py
@@ -0,0 +1,40 @@
+
+def suma(n1, n2):
+  sum= n1+n2
+  return sum
+print('ejemplo1')
+print(str(suma(34, 2)))
+print('ejemplo2')
+print(str(suma(3, 2)))
+
+def multiplicacion(n1, n2):
+  sum= n1*n2
+  return sum
+print('ejemplo1')
+print(str(multiplicacion(34, 2)))
+print('ejemplo2')
+print(str(multiplicacion(3, 2)))
+
+def resta(n1, n2):
+  sum= n1-n2
+  return sum
+print('ejemplo1')
+print(str(resta(34, 2)))
+print('ejemplo2')
+print(str(resta(3, 2)))
+
+def divicion(n1, n2):
+  sum= n1/n2
+  return sum
+print('ejemplo1')
+print(str(divicion(34, 2)))
+print('ejemplo2')
+print(str(divicion(3, 2)))
+
+
+
+
+
+
+
+
---
+++ main.js
@@ -0,0 +1,59 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+console.log("suma:");
+function suma(n1, n2) {
+  const result = n1 + n2;
+  return result;
+  //return n1 + n1
+}
+var resultado =suma (2 ,4);
+var resultado2 =suma (5,7);
+console.log("ejemplo 1:");
+console.log(resultado);
+console.log("ejemplo2:");
+console.log(resultado2);
+console.log("**********");
+console.log("resta:");
+function resta(n1, n2) {
+  const result = n1 - n2;
+  return result;
+  //return n1 + n1
+}
+var resultado =resta (2 ,4);
+var resultado2 =resta (4,7);
+console.log("ejemplo 1:");
+console.log(resultado);
+console.log("ejemplo2:");
+console.log(resultado2);
+
+console.log("**********");
+console.log("multiplicacion:");
+function multi (n1, n2) {
+  const result = n1 * n2;
+  return result;
+  //return n1 + n1
+}
+var resultado =multi (2 ,4);
+var resultado2 =multi (5,7);
+console.log("ejemplo 1:");
+console.log(resultado);
+console.log("ejemplo2:");
+console.log(resultado2);
+console.log("**********");
+
+console.log("divicion:");
+function divicion(n1, n2) {
+  const result = n1 / n2;
+  return result;
+  //return n1 + n1
+}
+var resultado =divicion (2 ,4);
+var resultado2 =divicion (5,7);
+console.log("ejemplo 1:");
+console.log(resultado);
+console.log("ejemplo2:");
+console.log(resultado2);
